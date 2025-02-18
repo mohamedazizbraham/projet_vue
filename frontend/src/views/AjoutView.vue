e2b8eda06167d0d5401833b2e2dff7d8b64ea9b5
mohamedazizbraham <mohamed_aziz.braham@etud.univ-jfc.fr>
2025-02-18 16:43:23 +0100

    Ajout dans Views de AjoutView

---
+++ frontend/src/views/AjoutView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about1">
+    <ParticipantProjet />
+
+  </div>
+</template>
+
+<script setup>
+
+import ParticipantProjet from "@/components/ParticipantProjet.vue";
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
